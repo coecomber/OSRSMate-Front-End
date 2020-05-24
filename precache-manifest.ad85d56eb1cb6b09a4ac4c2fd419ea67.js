052c7f8f023b03c8d17b6ee5c34e0784ad63eb0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-24 22:30:12 +0000

    Deploy coecomber/OSRSMate-Front-End to github.com/coecomber/OSRSMate-Front-End.git:gh-pages

---
+++ precache-manifest.ad85d56eb1cb6b09a4ac4c2fd419ea67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e6707a7d7d0258ef2a1aaf6d794040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d4a9a2af3447b4c4ef",
+    "url": "/static/css/main.fb2b7672.chunk.css"
+  },
+  {
+    "revision": "027f3aadd68c2378aa09",
+    "url": "/static/js/2.9a443ef3.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.9a443ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d4a9a2af3447b4c4ef",
+    "url": "/static/js/main.d9ff1f1a.chunk.js"
+  },
+  {
+    "revision": "18e12ea1237062c64eca",
+    "url": "/static/js/runtime-main.475625b2.js"
+  }
+]);
